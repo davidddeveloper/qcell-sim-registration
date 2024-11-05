6c8500d67812b8ce464e38207fbfb4039bd5a93a
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-11-05 12:41:24 +0000

    created customer database schema

--- sim_registration/customers/models.py
+++ sim_registration/customers/models.py
@@ -13,6 +13,18 @@ class IDType(models.Model):
     def __str__(self):
         return self.name
 
+class SimType(models.Model):
+    """
+        Represent ID TYPE
+
+        Attributes:
+            - name: name of id type (eg. passport)
+    """
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Customer(models.Model):
     """
         Represent a new Qcell customer
@@ -27,12 +39,16 @@ class Customer(models.Model):
             - address: customer address
             - nationality: customer nationality
     """
-    first_name = models.CharField("FirstName", max_length=50)
-    last_name = models.CharField("LastName", max_length=50)
-    mssisdn = models.CharField("Phone Number (mssidn number)", max_length=50)
+    sim_type = models.ForeignKey(to=SimType, verbose_name="Sim Type", blank=True, on_delete=models.CASCADE, default=["Passport"])
+    first_name = models.CharField("First Name", max_length=50)
+    last_name = models.CharField("Last Name", max_length=50)
+    mssisdn = models.CharField("MSSISDN Number", max_length=50)
     profession = models.CharField("Profession", max_length=100)
     id_number = models.CharField("ID Number", max_length=100)
-    id_type = models.ManyToManyField(to=IDType, blank=True)
+    id_type = models.ForeignKey(to=IDType, verbose_name="ID Type", blank=True, on_delete=models.CASCADE, default=["Passport"])
     id_picture = models.CharField("Photo of ID", max_length=200)
     address = models.CharField("Address", max_length=200)
     nationality = models.CharField("Nationality", max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
