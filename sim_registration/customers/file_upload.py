4a3a57ce3429d4e0d8c906b97f75c12ae698915b
davidddeveloper <programmingdonebydavid@gmail.com>
2024-11-08 08:27:47 -0800

    finished setting graphql file upload

--- sim_registration/customers/file_upload.py
+++ sim_registration/customers/file_upload.py
@@ -1,14 +1,36 @@
 from graphene_file_upload.scalars import Upload
 from graphene_file_upload.scalars import Upload
+from graphql_jwt.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+from werkzeug.utils import secure_filename
+from .models import Customer
 import graphene
+
+#def serialize_file(file):
+#    from django.core.files.storage import FileSystemStorage
+#    from django.conf import settings
+#    fs = FileSystemStorage(location=settings.STATIC_ROOT + '/uploads/')
+#    return fs.url(file)
 
 class UploadMutation(graphene.Mutation):
     class Arguments:
         file = Upload(required=True)
+        customer_id = graphene.String(required=True)
 
     success = graphene.Boolean()
+    uploaded_file_url = graphene.String()
 
-    def mutate(self, info, file, **kwargs):
+    @login_required
+    def mutate(self, info, file, customer_id, **kwargs):
         # do something with your file
-
-        return UploadMutation(success=True)
+        user = info.context.user
+        customer = Customer.objects.get(id=customer_id)
+        location = settings.MEDIA_ROOT + '/uploads/' + str(user.pk) + '/'
+        fs = FileSystemStorage(location=location)
+        filename = secure_filename(file.name)
+        filename = fs.save(filename, file)
+        uploaded_file_url = settings.MEDIA_URL + 'uploads/' + customer_id + '/' + filename
+        customer.id_picture = uploaded_file_url
+        customer.save()
+        return UploadMutation(success=True, uploaded_file_url=uploaded_file_url)
--- sim_registration/sim_registration/settings.py
+++ sim_registration/sim_registration/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-78g__#31wmq)8n0)$3gjr*2451vk5ok)9%7j0(#6+8m)hc+o92
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.100.181", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.100.181", "192.168.0.69", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "media/"
