6913982ac1f5d0d2d51b048a55e880089eef68a9
davidddeveloper <programmingdonebydavid@gmail.com>
2024-12-02 15:40:42 +0000

    added an ip to allowed hosts, added /media/ to .gitignore

--- sim_registration/sim_registration/settings.py
+++ sim_registration/sim_registration/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.100.181", "192.168.0.69", "localhost", "127.0.0.1", "192.168.100.180"]
+ALLOWED_HOSTS = ["192.168.100.181", "192.168.0.69", "localhost", "127.0.0.1", "192.168.100.180", "192.168.0.58"]
 
 
 # Application definition
